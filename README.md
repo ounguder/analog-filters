d5bd9c29bd6fdeedb6c41fcb79f5f1bcf0ea9fd5
ounguder <ounguder@gmail.com>
2022-07-17 15:30:37 +0200

    RC Low Pass is added

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # analog-filters
 Transfer functions, bode plots, zero plots of different type of Analog Filters are in this repository can be used as a study aid for Analog and Digital Signal Processing lecture
+
+
+## RC Low Pass Filter
+
---
+++ basic-filters/RC_low_pass.m
@@ -0,0 +1,25 @@
+clc;clear all;close all
+%% Values for the Circuit Elements
+R1= 1e3 ;
+C1= 1e-6;
+
+%% Transfer Function and System
+num = [1/(R1*C1)] ;
+denom=[1,1/(R1*C1)];
+sys = tf(num,denom);
+
+%% Bode Plot Options
+opts = bodeoptions;
+opts.Title.FontSize = 15;
+opts.Title.Color = 'blue';
+opts.Grid = 'on';
+opts.FreqScale = 'log';
+opts.Title.String = 'RC Low Pass Filter For R = 1kOhm C = 1\muF';
+
+%% Plotting the Bode Plot
+figure
+h = bodeplot(sys,opts);
+ 
+%% Plotting the Pole-Zero Plot
+figure
+pzplot(sys)
